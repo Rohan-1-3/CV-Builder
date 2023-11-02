35d4b9a21c2f5011b327700518eb2161aec2afd9
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-11-02 10:07:58 +0545

    added summary and fixed comma in personal details

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable react/prop-types */
 import "../cssFiles/Preview.css"
 
-function Preview({details}) {
+function Preview({ details }) {
+    const { fullName, address, phone, email } = details;
+
     return (
         <div className="cv-preview">
-            <div className="cv-preview-personal">
-                <h1>{details.fullName}</h1>
-                <p>{details.address}</p>
-                <p>{details.phone}, {details.email}</p>
+            <div className="cv-container">
+                <div className="cv-preview-personal">
+                    <h1>{fullName}</h1>
+                    <p>{address}</p>
+                    {phone !== "" || email !== "" ? (
+                        <p>{phone ? phone : ''}
+                        {phone !== '' && email !== '' ? ', ' : ''}
+                        {email ? email : ''}</p>
+                    ) : (
+                        <p></p>
+                    )}
+                </div>
+                <div className="professional-summary">
+                        <h3>Professional Summary</h3>
+                        <p>{details.summary}</p>
+                </div>
             </div>
         </div>
     );
